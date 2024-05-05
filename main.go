9e164b83b6368ec50571e3da24fd4cb95a3c8a6e
Ruvic Reyes <ruvicgreyes@yahoo.com>
2024-05-05 08:24:57 +0000

    On branch removeProd

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
-import (	
+import (
 	"log"
 )
 
-
 type Storer interface {
 	AddProduct(product *Product)
-	//RemoveProduct(id int)
+	RemoveProduct(id int)
 	//UpdateProduct(id int, product *Product)
 	//GetProduct(id int) *Product
 	GetAllProducts()
@@ -22,7 +21,7 @@ type Product struct {
 
 func (p Product) AddProduct(product *Product) {
 	// Load existing data from the JSON file
-	
+
 	existingData, err := loadData("database.json")
 	if err != nil {
 		log.Print(err, "\n We Create New one called database.json")
@@ -40,9 +39,33 @@ func (p Product) AddProduct(product *Product) {
 	}
 }
 
-// func (p Product) RemoveProduct(id int) {
+func (p Product) RemoveProduct(id int) {
 
-// }
+	existingData, err := loadData("database.json")
+	if err != nil {
+		panic(err)
+	}
+	var prod []Product
+	// Iterate over the items and remove the item with the specified ID
+	found := false
+	for _, item := range existingData {
+		if item.ID == id {
+			found = true
+			log.Println("ID NO:", id, "is completely removed")
+		} else {
+			prod = append(prod, item)
+		}
+	}
+
+	// If the item was not found, return an error
+	if !found {
+		log.Println("No ID Found")
+	}
+
+	//Update Database
+	writeData(prod)
+
+}
 
 // func (p Product) updatedProduct(product *Product) {
 
@@ -81,13 +104,14 @@ func main() {
 	// Add some products
 	// inventory.AddProduct(&Product{ID: 1, Name: "Laptop", Price: 1000, Quantity: 10})
 	// inventory.AddProduct(&Product{ID: 2, Name: "Smartphone", Price: 500, Quantity: 20})
+	// inventory.AddProduct(&Product{ID: 3, Name: "tablet", Price: 1500, Quantity: 20})
 
 	// List all products
 	log.Println("All Products:")
 	inventory.GetAllProducts()
 
 	// // Remove a product
-	// inventory.RemoveProduct(1)
+	inventory.RemoveProduct(1)
 
 	// // Update a product
 	// updatedProduct := &Product{ID: 2, Name: "Updated Smartphone", Price: 550, Quantity: 25}
@@ -98,7 +122,7 @@ func main() {
 	// fmt.Println("Updated Product:", product)
 }
 
-//Next is to list all Products
+//Next is to list remove product
 
 /* get list of product if == id
 https://stackoverflow.com/questions/34172001/how-to-retrieve-array-of-elements-from-array-of-structure-in-golang */
@@ -115,4 +139,3 @@ It allows practicing basic CRUD operations and managing data using in-memory sto
 It provides a practical context to apply concepts like composition, encapsulation, and abstraction in Go programming.
 Feel free to expand upon this project by adding more features like persistence using databases, validation, error handling, or implementing a web-based interface using frameworks like Gin or Echo.
 */
-
